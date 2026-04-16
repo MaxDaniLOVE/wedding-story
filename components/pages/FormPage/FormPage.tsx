42d56d2d47b9402bc4e6109a815936781b30b938
maks.danilau <maks.danilau@gmail.com>
2026-04-17 00:43:11 +0200

    Enhance FormPage styles

--- components/pages/FormPage/FormPage.tsx
+++ components/pages/FormPage/FormPage.tsx
@@ -57,14 +57,21 @@ export function FormPage() {
           <form className={styles.formWrapper} onSubmit={onSubmit}>
             <label htmlFor="input" className={styles.subtitle}>{invitedFriendInfo?.question}</label>
             <div className={`${styles.inputWrapper} ${hasError ? styles.error : ''}`}>
-              <input id='input' onChange={onChange} value={value} className={`${styles.input} ${hasError ? styles.error : ''}`} placeholder="Введите ответ" />
+              <input
+                autoComplete='off'
+                id='input'
+                onChange={onChange}
+                value={value}
+                className={`${styles.input} ${hasError ? styles.error : ''}`}
+                placeholder="Введите ответ"
+              />
               <button type="submit">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="22" viewBox="0 0 16 22" fill="none">
                   <path fillRule="evenodd" clipRule="evenodd" d="M0.818923 21.6855C1.75985 22.1806 3.13285 22.0802 3.88559 21.4615L15.5219 11.8963C16.1594 11.3723 16.1594 10.6277 15.5219 10.1037L3.88559 0.53853C3.13285 -0.0802326 1.75985 -0.180555 0.818923 0.314457C-0.122007 0.809467 -0.274562 1.71236 0.478182 2.33113L11.0241 11L0.478182 19.6689C-0.274562 20.2876 -0.122007 21.1905 0.818923 21.6855Z" fill="#FBFDFB"/>
                 </svg>
               </button>
             </div>
-            {hasError && <p>Неверно :(</p>}
+            <p className={hasError ? styles.errorLabel : ''}>Неверно :(</p>
           </form>
         </div>
     </section>
